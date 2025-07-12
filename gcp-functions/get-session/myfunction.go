2a5551612ad1477576a0d1ba4a5e64864606a550
AjayBhatta16 <ajay.bhattacharyya.16@gmail.com>
2025-07-11 22:38:50 -0400

    API: initial scaffold for getting new sessions

--- gcp-functions/get-session/myfunction.go
+++ gcp-functions/get-session/myfunction.go
@@ -1,10 +1,88 @@
 package myfunction
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+	"slices"
+	"strings"
+	"time"
+
+	"cloud.google.com/go/firestore"
+	"github.com/google/uuid"
 )
 
 func GetSession(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "GetSession - CI/CD pipeline succeeded")
+	ctx := context.Background()
+	dbClient, err := firestore.NewClient(ctx, "")
+
+	if err != nil {
+		http.Error(w, "Failed to create Firestore client", http.StatusInternalServerError)
+		return
+	}
+
+	defer dbClient.Close()
+
+	sessionID := GetSessionID(r.URL.Path)
+
+	if sessionID == "" {
+		session := GetDefaultSession()
+
+		session = saveSession(dbClient, session)
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(session)
+	} else {
+		fmt.Fprintf(w, "GetSession - %s", sessionID)
+	}
+}
+
+func GetSessionID(url string) string {
+	tokens := strings.Split(url, "/")
+	slices.Reverse(tokens)
+	lastToken := tokens[0]
+
+	if lastToken == "get-session" || lastToken == "get-session/" {
+		return ""
+	}
+
+	return lastToken
+}
+
+func GetDefaultSession() Session {
+	file, err := os.Open("default-session.json")
+	if err != nil {
+		fmt.Println("Error opening default session file:", err)
+		return Session{}
+	}
+	defer file.Close()
+
+	var session Session
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&session); err != nil {
+		fmt.Println("Error decoding default session:", err)
+		return Session{}
+	}
+
+	session.LastUpdatedTimeStamp = time.Now().UnixMilli()
+	session.SessionId = uuid.NewString()
+
+	return session
+}
+
+func saveSession(client *firestore.Client, session Session) Session {
+	ctx := context.Background()
+
+	_, _, err := client.Collection("your-collection").Add(ctx, map[string]interface{}{
+		"hello": "world",
+	})
+
+	if err != nil {
+		fmt.Println("Firestore write error", http.StatusInternalServerError)
+		return Session{}
+	}
+
+	return session
 }
---
+++ gcp-functions/get-session/session.go
@@ -0,0 +1,31 @@
+package myfunction
+
+type Session struct {
+	SessionId            string            `json:"sessionId"`
+	LastUpdatedTimeStamp int64             `json:"lastUpdatedTimeStamp"`
+	LastGuessFeedback    string            `json:"lastGuessFeedback"`
+	GuessSequence        []string          `json:"guessSequence"`
+	Correct              CorrectLetters    `json:"correct"`
+	Misplaced            []MisplacedLetter `json:"misplaced"`
+	Incorrect            []IncorrectLetter `json:"incorrect"`
+	NotWords             []string          `json:"notWords"`
+}
+
+type CorrectLetters struct {
+	Ch_1 string `json:"ch_1"`
+	Ch_2 string `json:"ch_2"`
+	Ch_3 string `json:"ch_3"`
+	Ch_4 string `json:"ch_4"`
+	Ch_5 string `json:"ch_5"`
+}
+
+type MisplacedLetter struct {
+	Value     string `json:"value"`
+	Pos       int    `json:"pos"`
+	Duplicate bool   `json:"duplicate"`
+}
+
+type IncorrectLetter struct {
+	Value     string `json:"value"`
+	Duplicate bool   `json:"duplicate"`
+}
