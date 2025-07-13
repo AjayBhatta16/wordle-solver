5fb099b3bb3afe5d0173ba0859630291ff95d17b
AjayBhatta16 <ajay.bhattacharyya.16@gmail.com>
2025-07-13 03:02:22 -0400

    fixed handler call

--- gcp-functions/compute-next-word/index.js
+++ gcp-functions/compute-next-word/index.js
@@ -3,7 +3,7 @@ const ComputeNextWordHandler = require('./handler.js');
 exports.computeNextWord = (req, res) => {
   const handler = new ComputeNextWordHandler();
 
-  const response = handler.handle(req.body);
+  const response = handler.handle({ session: req.body });
 
   res.setHeader('Content-Type', 'application/json');
   res.status(200).send(JSON.stringify(response, null, 2));
