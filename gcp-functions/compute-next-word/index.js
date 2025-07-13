a845a6c55000553823001575de8335e2c79758c7
AjayBhatta16 <ajay.bhattacharyya.16@gmail.com>
2025-07-13 02:58:19 -0400

    removed unnecessary JSON parse

--- gcp-functions/compute-next-word/index.js
+++ gcp-functions/compute-next-word/index.js
@@ -3,9 +3,7 @@ const ComputeNextWordHandler = require('./handler.js');
 exports.computeNextWord = (req, res) => {
   const handler = new ComputeNextWordHandler();
 
-  const handlerRequest = JSON.parse(req.body ?? '{}');
-
-  const response = handler.handle(handlerRequest);
+  const response = handler.handle(req.body);
 
   res.setHeader('Content-Type', 'application/json');
   res.status(200).send(JSON.stringify(response, null, 2));
