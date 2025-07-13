9dadbd6416331831bf5d8c0cd8c96421a053a5a5
AjayBhatta16 <ajay.bhattacharyya.16@gmail.com>
2025-07-12 22:26:11 -0400

    fixed last issue for subtype structs

--- gcp-functions/get-session/session.go
+++ gcp-functions/get-session/session.go
@@ -13,20 +13,20 @@ type Session struct {
 }
 
 type CorrectLetters struct {
-	Ch_1 string `json:"ch_1"`
-	Ch_2 string `json:"ch_2"`
-	Ch_3 string `json:"ch_3"`
-	Ch_4 string `json:"ch_4"`
-	Ch_5 string `json:"ch_5"`
+	Ch_1 string `json:"ch_1" firestore:"ch_1"`
+	Ch_2 string `json:"ch_2" firestore:"ch_2"`
+	Ch_3 string `json:"ch_3" firestore:"ch_3"`
+	Ch_4 string `json:"ch_4" firestore:"ch_4"`
+	Ch_5 string `json:"ch_5" firestore:"ch_5"`
 }
 
 type MisplacedLetter struct {
-	Value     string `json:"value"`
-	Pos       int    `json:"pos"`
-	Duplicate bool   `json:"duplicate"`
+	Value     string `json:"value" firestore:"value"`
+	Pos       int    `json:"pos" firestore:"pos"`
+	Duplicate bool   `json:"duplicate" firestore:"duplicate"`
 }
 
 type IncorrectLetter struct {
-	Value     string `json:"value"`
-	Duplicate bool   `json:"duplicate"`
+	Value     string `json:"value" firestore:"value"`
+	Duplicate bool   `json:"duplicate" firestore:"duplicate"`
 }
