7826469bc937a83b8a80e0118da047fd9c22a5c7
AjayBhatta16 <ajay.bhattacharyya.16@gmail.com>
2025-07-21 23:14:47 -0400

    initial scaffold for cleanup timer

--- gcp-functions/cleanup-timer/myfunction.go
+++ gcp-functions/cleanup-timer/myfunction.go
@@ -3,6 +3,10 @@ package myfunction
 import (
 	"context"
 	"log"
+	"os"
+	"time"
+
+	"cloud.google.com/go/firestore"
 )
 
 type PubSubMessage struct {
@@ -10,7 +14,65 @@ type PubSubMessage struct {
 }
 
 func CleanupTimer(ctx context.Context, m PubSubMessage) error {
-	log.Println("CleanupTimer - CI/CD pipeline succeeded")
+	log.Println("CleanupTimer - start")
 
+	dbClient := getDBClient()
+
+	defer dbClient.Close()
+
+	expiredSessions := getExpiredDocRefIDs(dbClient)
+
+	for _, docID := range expiredSessions {
+		err := deleteFirestoreDoc(dbClient, docID)
+		if err != nil {
+			log.Printf("CleanupTimer - Error deleting document %s: %v", docID, err)
+		} else {
+			log.Printf("CleanupTimer - Successfully deleted document %s", docID)
+		}
+	}
+
+	return nil
+}
+
+func getDBClient() *firestore.Client {
+	ctx := context.Background()
+	dbClient, err := firestore.NewClient(ctx, os.Getenv("PROJECT_ID"))
+
+	if err != nil {
+		log.Println("getDBClient - Error creating Firestore client:", err)
+		return nil
+	}
+
+	return dbClient
+}
+
+func getExpiredDocRefIDs(dbClient *firestore.Client) []string {
+	ctx := context.Background()
+
+	minTimestamp := time.Now().AddDate(0, 0, -7).UnixMilli()
+
+	iter := dbClient.Collection("sessions").Where("lastUpdatedTimeStamp", "<", minTimestamp).Documents(ctx)
+
+	var docIDs []string
+
+	for {
+		doc, err := iter.Next()
+		if err != nil {
+			break
+		}
+		docIDs = append(docIDs, doc.Ref.ID)
+	}
+
+	return docIDs
+}
+
+func deleteFirestoreDoc(dbClient *firestore.Client, docID string) error {
+	ctx := context.Background()
+	_, err := dbClient.Collection("sessions").Doc(docID).Delete(ctx)
+	if err != nil {
+		log.Printf("deleteFirestoreDoc - Error deleting document %s: %v", docID, err)
+		return err
+	}
+	log.Printf("deleteFirestoreDoc - Successfully deleted document %s", docID)
 	return nil
 }
